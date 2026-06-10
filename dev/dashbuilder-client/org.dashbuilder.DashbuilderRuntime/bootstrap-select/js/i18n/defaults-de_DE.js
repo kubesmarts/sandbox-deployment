a7cebf424da92c2ae59071b3465d609678fc829b
kie-ci <kie-ci0@redhat.com>
2026-06-10 08:10:15 +0000

    Deploy 0.0.0 (dev) using tag: main

--- dev/dashbuilder-client/org.dashbuilder.DashbuilderRuntime/bootstrap-select/js/i18n/defaults-de_DE.js
+++
@@ -1,2 +0,0 @@
-/*! For license information please see defaults-de_DE.js.LICENSE.txt */
-!function(e,t){"function"==typeof define&&define.amd?define(["jquery"],function(e){return t(e)}):"object"==typeof exports?module.exports=t(require("jquery")):t(jQuery)}(0,function(e){e.fn.selectpicker.defaults={noneSelectedText:"Bitte wählen...",noneResultsText:"Keine Ergebnisse für {0}",countSelectedText:"{0} von {1} ausgewählt",maxOptionsText:["Limit erreicht ({n} {var} max.)","Gruppen-Limit erreicht ({n} {var} max.)",["Eintrag","Einträge"]],multipleSeparator:", "}});
